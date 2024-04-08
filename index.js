dc05d7e2c264f58730fb7d60a8ef66a0de4da268
Lucy Kariuki <kariuki.lucy@strathmore.edu>
2024-04-08 22:15:33 +0300

    Create event listeners using addEventListener method

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
+
 function addingEventListener() {
+    const input = document.getElementById("button");// fetching the html document
+    input.addEventListener('click',function() {
+        alert('I\'m clicked!');
+    })
 }
+
+// call the function to set up the eventlistener
+addingEventListener();
